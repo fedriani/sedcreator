68b0fede75978bb05de7f2bfeb5811a525822439
fedriani <ruben.fedriani@gmail.com>
2022-07-27 12:20:07 +0200

    Update __init__.py
    adding checks for input arrays

--- sedcreator/__init__.py
+++ sedcreator/__init__.py
@@ -964,6 +964,28 @@ class SedFitter(object):
                  upper_limit_array=None,
                  filter_array=None):
 
+        #Checks to ensure inputs are correct
+        if any(lambda_array<0):
+            raise ValueError("A value in the lambda array is negative, please check")
+        if any(np.isnan(lambda_array)):
+            raise ValueError("A value in the lambda array is nan, please check")
+            
+        if any(flux_array<0):
+            raise ValueError("A value in the flux array is negative, please check")
+        if any(np.isnan(flux_array)):
+            raise ValueError("A value in the flux array is nan, please check")
+
+        if any(np.isnan(err_flux_array)):
+            raise ValueError("A value in the error flux array is nan, please check")
+        
+        #Check length of arrays are the same
+        #based on: https://stackoverflow.com/questions/35791051/better-way-to-check-if-all-lists-in-a-list-are-the-same-length
+        lists = (lambda_array,flux_array,err_flux_array,upper_limit_array,filter_array)
+        it = iter(lists)
+        the_len = len(next(it))
+        if not all(len(l) == the_len for l in it):
+            raise ValueError('not all arrays have same length!, please check')
+
 
         self.lambda_array = lambda_array
         self.flux_array = flux_array
